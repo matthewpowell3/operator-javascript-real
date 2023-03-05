1a254ca825788ae7c8da8a991997efb9f270ce66
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 12:08:59 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-var x = 8;
+/*var x = 8;
 var y = 3;
 
-// document.write('Addition', x + y);
-// document.write('Subtract', x - y);
-// document.write('Multiple', x * y);
+document.write('Addition', x + y);
+document.write('Subtract', x - y);
+document.write('Multiple', x * y);
 
-// Difference with modulus and divsion
+Difference with modulus and divsion
 
-// division gives you the quotient answer
+Division gives you the quotient answer
 document.write('Division', x / y);
 
-//modulus gives you the remainder
+Modulus gives you the remainder
 document.write('Modulus', x % y);
 
 var z = 5;
-//  incerment increases by one
+ Incerment increases by one
 document.write('Increment', ++z);
-// //  decerment decreases by one
+ Decerment decreases by one
 document.write('Decrement', --z);
+*/
+/*
+var a = 13;
+var b = a;
+document.write('Value of b ', b);
+
+var b1 = 50;
+// value = 50
+b1 += 10;
+// value = 60
+b1 = b1 + 10;
+// value = 70
+document.write('Value of b1 ', b1);
+
+b1 -= 5;
+//  value = 65
+document.write('Value of b1 ', b1);
+
+a3 = 10;
+a3 *= 10;
+document.write('Value of a3 ', a3);
+
+a4 = 10;
+a4 /= 10;
+document.write('Value of a4 ', a4);
+
+a5 = 10;
+a5 %= 10;
+document.write('Value of a5', a5);
+*/
+
+var firstName = 'Matthew';
+var lastName = 'Powell';
