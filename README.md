222ec1f72494ee752e7e6c575793012992ba9f9c
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 11:43:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# operator-javascript-real
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kzbfbi)
---
+++ script.js
@@ -0,0 +1,19 @@
+var x = 8;
+var y = 3;
+
+// document.write('Addition', x + y);
+// document.write('Subtract', x - y);
+// document.write('Multiple', x * y);
+
+// Difference with modulus and divsion
+
+// division gives you the quotient answer
+document.write('Division', x / y);
+
+//modulus gives you the remainder
+document.write('Modulus', x % y);
+
+var z = 5;
+//  incerment increase by one
+document.write('Increment', ++z);
+document.write('Decrement', --z);
